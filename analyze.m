4d794b1e1e9a4fffd8db9736920994382b9465c0
Zane Martin <zkm2102@columbia.edu>
2016-09-02 13:11:09 -0400

    Added ability to plot mean inflammation for all .csv files in directory

--- analyze.m
+++ analyze.m
@@ -1,9 +1,21 @@
 % This creates a script for the Software Carpentry Workshop playing with
-% fictional patient data
+% fictional patient data. It reads in the mean patient data from each of
+% the 12 inflammation files and then plots on a single figure all 12
+% patients.
+
+Legend = cell(12,1);
+for i=1:12
+    patient_data = csvread(sprintf('inflammation-%02d.csv', i));
+    mean_patient_data = mean(patient_data);
+    hold on;
+    plot(mean_patient_data);
+    xlabel('Days');
+    ylabel('Mean Inflammation');
+    Legend{i} = sprintf('Mean Inflammation No.%02d', i);
+end
+    legend(Legend);
 
 
-patient_data = csvread('inflammation-01.csv');
-mean_patient_data = mean(patient_data);
-plot(mean_patient_data);
-xlabel('Days');
-ylabel('Mean Inflammation');
+    
+
+ 
